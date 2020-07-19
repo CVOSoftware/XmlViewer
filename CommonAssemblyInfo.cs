9f1b1f09fb8a4220317d080b34798a06ef219210
cvosoftware <cvosoftware@gmail.com>
2020-07-19 19:08:53 +0300

    (fix): Fixed border colors

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("XmlViewer")]
 [assembly: AssemblyCopyright("Vlad Chernikov")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2020.1.0.0")]
+[assembly: AssemblyFileVersion("2020.1.0.0")]
